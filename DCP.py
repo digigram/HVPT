dfe2d8d1017f27e12c2c5e188a3ce6228ef7b3ae
DigiGram <swartmc@gmail.com>
2014-03-23 19:47:48 +0100

    fixed inconsistant indentation

--- DCP.py
+++ DCP.py
@@ -18,11 +18,11 @@ initCount = 0 #### *1
 #####
 
 def average(arr):
-    total = 0
-    avg = 0
-    for n in arr:
-        total += n
-    return total/len(arr)
+  total = 0
+  avg = 0
+  for n in arr:
+      total += n
+  return total/len(arr)
     
 def tendency(arr):
   arr2 = arr[2:len(arr)]
@@ -44,18 +44,18 @@ def diffImg(img0, img1, img2):
   return cv2.bitwise_and(dif1, dif2)
 
 def currentLevelElevator():
-    #this will call the Arduino
-    #lvls = serial.Serial.read()
-    #read from Arduino: level currently on / level going to
-    #3/1
-    global nowL # *1
-    global goL # *1
-    lvls = nowL + '/' + goL #### see above *1
-    currentLevel = lvls.split('/')[0]
-    goingtoLevel = ''
-    if len(lvls.split('/')[1]) > 0:
-      goingtoLevel = lvls.split('/')[1]
-    return currentLevel, goingtoLevel
+  #this will call the Arduino
+  #lvls = serial.Serial.read()
+  #read from Arduino: level currently on / level going to
+  #3/1
+  global nowL # *1
+  global goL # *1
+  lvls = nowL + '/' + goL #### see above *1
+  currentLevel = lvls.split('/')[0]
+  goingtoLevel = ''
+  if len(lvls.split('/')[1]) > 0:
+    goingtoLevel = lvls.split('/')[1]
+  return currentLevel, goingtoLevel
     
 #initialise
 cam = [0]
@@ -66,136 +66,135 @@ camLevel=['1']
 
 #find and attach all connected cameras
 for camNr in range(0,5):
-    print camNr
-    iscam = cv2.VideoCapture(camNr)
-    print iscam.isOpened()
-    if iscam.isOpened():
-        allCam.append(iscam)
-        
+  print camNr
+  iscam = cv2.VideoCapture(camNr)
+  print iscam.isOpened()
+  if iscam.isOpened():
+    allCam.append(iscam)
+      
        
 #iterate over all cameras
 #This only works for 1 cam for now. This should be inside the while loop
 for camNR in range(0, len(allCam)):
-    title = "Defocused Computer Perception - Level: " + str(camLevel[camNR])
-    cv2.namedWindow(title, cv2.CV_WINDOW_AUTOSIZE)
+  title = "Defocused Computer Perception - Level: " + str(camLevel[camNR])
+  cv2.namedWindow(title, cv2.CV_WINDOW_AUTOSIZE)
 
-    # Read three images first:
-    img_prev = cv2.flip(cv2.cvtColor(allCam[camNR].read()[1], cv2.COLOR_RGB2GRAY), 1)
-    img = cv2.flip(cv2.cvtColor(allCam[camNR].read()[1], cv2.COLOR_RGB2GRAY), 1)
+  # Read three images first:
+  img_prev = cv2.flip(cv2.cvtColor(allCam[camNR].read()[1], cv2.COLOR_RGB2GRAY), 1)
+  img = cv2.flip(cv2.cvtColor(allCam[camNR].read()[1], cv2.COLOR_RGB2GRAY), 1)
+  img_fut = cv2.flip(cv2.cvtColor(allCam[camNR].read()[1], cv2.COLOR_RGB2GRAY), 1)
+
+  #It's called last100 but can be any size. It's dynamic
+  sizeOfMotionHistory = 10
+  last100 = []
+  for i in range(0,sizeOfMotionHistory):
+      last100.append(0)
+  x0 = 0
+  y0 = 0
+  motion = 'None'
+
+  while True:
+    dimg = diffImg(img_prev, img, img_fut)
+    #print cv2.countNonZero(dimg) #to calibrate pThresh once-off
+    pThresh = 150000
+    mThresh = 50000
+    
+    if cv2.countNonZero(dimg) > pThresh:
+      moments = cv2.moments(dimg, 0) 
+      area = moments['m00'] 
+      #print area #to calibrate mThresh once-off
+      if(area > mThresh):  
+        x = int(abs(moments['m10'] / area))
+        y = int(abs(moments['m01'] / area))
+        dist = np.sqrt((x-x0)**2+(y-y0)**2)
+        last100 = last100[1:len(last100)]
+        last100.append(dist)
+        if tendency(last100) == '+':
+        #if last100[len(last100)-1]>average(last100):
+          motionPrev = motion
+          motion = 'Coming'
+        elif tendency(last100) == '-':
+        #if last100[len(last100)-1]<average(last100):
+          motionPrev = motion
+          motion = 'Going'
+        #print average(last100)
+        #if abs(average(last5) - last5[4]) < 5:
+        #    motionPrev = motion
+        #    motion = 'None'    
+        
+        #if motionPrev != motion: #If you want to tag all motion
+        #I just want to tag incoming persons
+        if motion == 'Coming':
+          currLvl, goingLvl = currentLevelElevator()
+          if (str(currLvl) != str(camLevel[camNR])) and (str(goingLvl) != str(camLevel[camNR])):
+            initCount += 1 # *1
+            if initCount > 10: # *1  
+              #print str(x)+';'+str(y)
+              passenger += 1
+              
+              global goL #### see above *1
+              goL = str(camLevel[camNR]) #### see above *1
+              
+              if (passenger < 42):
+                print str(camLevel[camNR]) #this becomes a serial.Serial.write()
+              elif (passenger == 42):
+                print str(0) 
+                #send the HVPT to the basement for councelling
+              if (passenger > 44):
+                passenger = 0
+                #the HVPT will go into councelling, so it needs some time. 
+                #Customers will have to wait and chit chat...
+         
+        thresholdedImage = dimg
+        grayscaleOriginal = img
+        
+        #Do you want the crosshair on a thresholded image
+        ##showimg = thresholdedImage 
+        #or on the grayscale original?
+        showimg = grayscaleOriginal
+        
+        #Here you can choose to have a croshair (2 line statements)    
+        cv2.line(showimg, (x-10, y), (x+10, y), 255)
+        cv2.line(showimg, (x, y-10), (x, y+10), 255)
+        #Or a circle        
+        ##cv2.circle(showimg, (x, y),20,255,5)
+        
+        #How you want to tag the motion
+        text = 'Passenger: ' + motion
+        if x < 0.5*len(dimg):
+          xtext = x - 25
+          ytext = y - 25
+        else:
+          xtext = x - 250
+          ytext = y - 25
+        cv2.putText(showimg, text, (xtext,ytext), cv2.FONT_HERSHEY_SIMPLEX, 1, 255) 
+      
+      lvlname = 'Level ' + str(camLevel[camNR])
+      cv2.putText(showimg, lvlname, (25,25), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)   
+      currLvl = 'Elevator: ' + str(currentLevelElevator()[0])
+      cv2.putText(showimg, currLvl, (len(showimg)-35, 25),  cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+      currLvl = 'Going to: ' + str(currentLevelElevator()[1])
+      cv2.putText(showimg, currLvl, (len(showimg)-35, 60),  cv2.FONT_HERSHEY_SIMPLEX, 1, 255)        
+      cv2.imshow( title, showimg )
+
+    ##### testing taking the tag away
+    lvlname = 'Level ' + str(camLevel[camNR])
+    cv2.putText(img, lvlname, (25,25), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)   
+    currLvl = 'Elevator: ' + str(currentLevelElevator()[0])
+    cv2.putText(img, currLvl, (len(img)-35, 25),  cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+    currLvl = 'Going to: ' + str(currentLevelElevator()[1])
+    cv2.putText(img, currLvl, (len(img)-35, 60),  cv2.FONT_HERSHEY_SIMPLEX, 1, 255)        
+    cv2.imshow( title, img )
+    #####
+    
+    # Read next image
+    img_prev = img
+    img = img_fut
     img_fut = cv2.flip(cv2.cvtColor(allCam[camNR].read()[1], cv2.COLOR_RGB2GRAY), 1)
 
-    #It's called last100 but can be any size. It's dynamic
-    sizeOfMotionHistory = 10
-    last100 = []
-    for i in range(0,sizeOfMotionHistory):
-        last100.append(0)
-    x0 = 0
-    y0 = 0
-    motion = 'None'
- 
-    while True:
-      dimg = diffImg(img_prev, img, img_fut)
-      #print cv2.countNonZero(dimg) #to calibrate pThresh once-off
-      pThresh = 150000
-      mThresh = 50000
-      
-      if cv2.countNonZero(dimg) > pThresh:
-        moments = cv2.moments(dimg, 0) 
-        area = moments['m00'] 
-        #print area #to calibrate mThresh once-off
-        if(area > mThresh):  
-            x = int(abs(moments['m10'] / area))
-            y = int(abs(moments['m01'] / area))
-            dist = np.sqrt((x-x0)**2+(y-y0)**2)
-            last100 = last100[1:len(last100)]
-            last100.append(dist)
-            if tendency(last100) == '+':
-            #if last100[len(last100)-1]>average(last100):
-                motionPrev = motion
-                motion = 'Coming'
-            elif tendency(last100) == '-':
-            #if last100[len(last100)-1]<average(last100):
-                motionPrev = motion
-                motion = 'Going'
-            #print average(last100)
-            #if abs(average(last5) - last5[4]) < 5:
-            #    motionPrev = motion
-            #    motion = 'None'    
-            
-            #if motionPrev != motion: #If you want to tag all motion
-            #I just want to tag incoming persons
-            if motion == 'Coming':
-                currLvl, goingLvl = currentLevelElevator()
-                if (str(currLvl) != str(camLevel[camNR])) and (str(goingLvl) != str(camLevel[camNR])):
-                  initCount += 1 # *1
-                  if initCount > 10: # *1
-                    
-                    #print str(x)+';'+str(y)
-                    passenger += 1
-                    
-                    global goL #### see above *1
-                    goL = str(camLevel[camNR]) #### see above *1
-                    
-                    if (passenger < 42):
-                        print str(camLevel[camNR]) #this becomes a serial.Serial.write()
-                    elif (passenger == 42):
-                        print str(0) 
-                        #send the HVPT to the basement for councelling
-                    if (passenger > 44):
-                        passenger = 0
-                        #the HVPT will go into councelling, so it needs some time. 
-                        #Customers will have to wait and chit chat...
-             
-            thresholdedImage = dimg
-            grayscaleOriginal = img
-            
-            #Do you want the crosshair on a thresholded image
-            ##showimg = thresholdedImage 
-            #or on the grayscale original?
-            showimg = grayscaleOriginal
-            
-            #Here you can choose to have a croshair (2 line statements)    
-            cv2.line(showimg, (x-10, y), (x+10, y), 255)
-            cv2.line(showimg, (x, y-10), (x, y+10), 255)
-            #Or a circle        
-            ##cv2.circle(showimg, (x, y),20,255,5)
-            
-            #How you want to tag the motion
-            text = 'Passenger: ' + motion
-            if x < 0.5*len(dimg):
-                xtext = x - 25
-                ytext = y - 25
-            else:
-                xtext = x - 250
-                ytext = y - 25
-            cv2.putText(showimg, text, (xtext,ytext), cv2.FONT_HERSHEY_SIMPLEX, 1, 255) 
-        
-        lvlname = 'Level ' + str(camLevel[camNR])
-        cv2.putText(showimg, lvlname, (25,25), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)   
-        currLvl = 'Elevator: ' + str(currentLevelElevator()[0])
-        cv2.putText(showimg, currLvl, (len(showimg)-35, 25),  cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
-        currLvl = 'Going to: ' + str(currentLevelElevator()[1])
-        cv2.putText(showimg, currLvl, (len(showimg)-35, 60),  cv2.FONT_HERSHEY_SIMPLEX, 1, 255)        
-        cv2.imshow( title, showimg )
+    key = cv2.waitKey(10)
+    if key == 27:
+      cv2.destroyWindow(title)
+      break
 
-      ##### testing taking the tag away
-      lvlname = 'Level ' + str(camLevel[camNR])
-      cv2.putText(img, lvlname, (25,25), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)   
-      currLvl = 'Elevator: ' + str(currentLevelElevator()[0])
-      cv2.putText(img, currLvl, (len(img)-35, 25),  cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
-      currLvl = 'Going to: ' + str(currentLevelElevator()[1])
-      cv2.putText(img, currLvl, (len(img)-35, 60),  cv2.FONT_HERSHEY_SIMPLEX, 1, 255)        
-      cv2.imshow( title, img )
-      #####
-      
-      # Read next image
-      img_prev = img
-      img = img_fut
-      img_fut = cv2.flip(cv2.cvtColor(allCam[camNR].read()[1], cv2.COLOR_RGB2GRAY), 1)
-
-      key = cv2.waitKey(10)
-      if key == 27:
-        cv2.destroyWindow(title)
-        break
-
-    print "Goodbye"
+  print "Goodbye"
